680d3c04a88aeb9cef28a352ca8a91bfaecb4767
qf-wang <sz-wangjing@1000phone.com>
2018-10-04 15:56:17 +0800

    图片懒加载和预加载

--- lazy_load/js/ajax2.0.js
+++ lazy_load/js/ajax2.0.js
@@ -1,39 +1,8 @@
-~function () {
-  //->getXHR:创建AJAX对象(使用惰性思想进行封装,兼容所有的浏览器的)
-  var getXHR = function () {
-      var flag = false, xhr = null, ary = [function () {
-          return new XMLHttpRequest;
-      }, function () {
-          return new ActiveXObject("Microsoft.XMLHTTP");
-      }, function () {
-          return new ActiveXObject("Msxml2.XMLHTTP");
-      }, function () {
-          return new ActiveXObject("Msxml3.XMLHTTP");
-      }];
-      for (var i = 0, len = ary.length; i < len; i++) {
-          var temp = ary[i];
-          try {
-              xhr = temp();
-              getXHR = temp;
-              flag = true;
-              break;
-          } catch (e) {
 
-          }
-      }
-      if (!flag) {
-          throw new Error("your browser is not support ajax~");
-      }
-      return xhr;
-  };
-
-  //->sendAJAX:发送AJAX请求的方法
-  //options:它是一个对象数据类型的值,把需要传递进来的参数和值都已键值对的方式传递进来
-  var sendAJAX = function (options) {
+  var sendAjax = function (url, options) {
       //->参数初始化
       var _default = {
-          url: "",
-          type: "get",
+          method: "get",
           async: true,
           data: null,
           success: null
@@ -43,23 +12,26 @@
               _default[key] = options[key];
           }
       }
-
       //->发送请求
-      var xhr = getXHR();
-      if (_default["type"].toLowerCase() === "get") {
-          //->GET请求还需要在URL的末尾追加随机数
-          var suffix = _default["url"].indexOf("?") > -1 ? "&" : "?";
-          _default["url"] += suffix + "_=" + Math.random();
+      var xhr = new XMLHttpRequest();
+      if (_default["method"].toLowerCase() === "get") {
+          var suffix = url.indexOf("?") > -1 ? "&" : "?";
+          url += suffix + "_=" + Date.now();
+        //   拼接url
+        for(var i in _default["data"]) {
+            url += '&' + i + '=' + _default.data[i];
+        }
+        _default.data = null;
+      } else {
+        _default.data = JSON.stringify(_default.data);
       }
-      xhr.open(_default["type"], _default["url"], _default["async"]);
+
+      xhr.open(_default["method"], url, _default["async"]);
       xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4 && /^2\d{2}$/.test(xhr.status)) {
-              var data = "JSON" in window ? JSON.parse(xhr.responseText) : eval("(" + xhr.responseText + ")");
+          if (xhr.readyState === 4 && xhr.status == 200) {
+              var data = xhr.responseText;
               _default["success"] && _default["success"](data);
           }
       };
       xhr.send(_default["data"]);
   };
-
-  window.sendAJAX = sendAJAX;
-}();
--- lazy_load/js/re_lasy_load.js
+++ lazy_load/js/re_lasy_load.js
@@ -5,47 +5,123 @@ function LazyLoad(ele, page, pageSize, width) {
     this.pageSize = pageSize || 40; //每页显示的数量
     // 图片的宽度
     this.width = width || 200;
+    this.arr = [];
     // 防止手机
     this.load = true;
+    this.timer = null;
 }
 // 初始化函数  参数：查询字段
-LazyLoad.prototype.init = function(words) {
+LazyLoad.prototype.init = function (words) {
+    var _this = this;
     // 清空盒子内的所有元素， 重置列数
     this.resize();
     // 获取图片
     this.getImage(words);
 
+    this.imageBox.addEventListener('scroll', function () {
+        var bottom = this.scrollHeight - this.scrollTop - this.clientHeight;
+        // console.log(bottom)
+        if (bottom < 80 && _this.load) {
+            _this.load = false;
+            _this.page++;
+            _this.getImage(words);
+        }
+    }, false)
+    window.addEventListener('resize', function() {
+        _this.resize();
+        clearInterval(_this.timer);
+        // 把现有的图片重新排版
+        _this.timer = setTimeout(function() {
+            _this.insertImage(_this.arr);
+        }, 200)
+    })
 
 }
 // 根据容器的宽度自适应显示列数
-LazyLoad.prototype.resize = function() {
+LazyLoad.prototype.resize = function () {
     var arr = []
-    for(var i = 0; i < Math.floor(this.imageBox.clientWidth / this.width); i++) {
+    for (var i = 0; i < Math.floor(this.imageBox.clientWidth / this.width); i++) {
         arr.push('<li></li>');
     }
     this.imageBox.innerHTML = arr.join('');
     arr = null;
 }
 // 获取图片资源
-LazyLoad.prototype.getImage = function(words) {
+LazyLoad.prototype.getImage = function (words) {
     var _this = this;
     var params = {
-        page_num: this.page,
-        page_size: this.pageSize,
-        q: words,
-        succees: function(data) {
-            _this.loadImage(data);
+        data: {
+            page_num: this.page,
+            page_size: this.pageSize,
+            q: words,
+        },
+        success: function (data) {
+            data = JSON.parse(data);
+            _this.loadImage(data.data);
         }
     }
-    sendAJAX("http://dev.91jianke.com:1091/id_v2_5/search_img", params);
+    sendAjax("http://dev.91jianke.com:1091/id_v2_5/search_img", params);
 }
 // 把图片加入到dom中
-LazyLoad.prototype.insertImage = function() {
-
+LazyLoad.prototype.insertImage = function (arrIamge) {
+    console.log(arrIamge)
+    var liAll = this.imageBox.querySelectorAll('li');
+    for (var index = 0; index < arrIamge.length; index++) {
+        var div = document.createElement('div');
+        div.className = 'img-list'
+        div.style.width = 200 + 'px';
+        div.style.height = 200 / arrIamge[index].width * arrIamge[index].height + 'px';
+        let _index = this.countHeight(liAll);
+        // debugger
+        div.appendChild(arrIamge[index]);
+        liAll[_index].appendChild(div);
+        liAll[_index].style.height = parseInt(liAll[_index].style.height) + parseInt(div.style.height) + 'px'
+      }
+      this.load =  true;
+}
+// 计算高度最小的列
+LazyLoad.prototype.countHeight = function(liAll) {
+    var arr = []
+    for(var i = 0; i < liAll.length; i++) {
+      // 由于flex布局的特殊性，li默认高度100%， 所以要动态给他设置高度
+      liAll[i].style.height = liAll[i].style.height ? liAll[i].style.height : 0
+      const obj = {
+        index: i,
+        height: parseInt(liAll[i].style.height)
+      }
+      arr.push(obj);
+    }
+    arr.sort(function(a, b){
+      return a.height -b.height;
+    })
+    console.log(arr);
+    return arr[0].index
 }
 
 // 图片预加载
-LazyLoad.prototype.loadImage = function(data) {
+LazyLoad.prototype.loadImage = function (data) {
+    var _this = this;
     // 处理图片跨域或者加载失败的情况
-    this.insertImage(data);
-} 
+    var arr = [];
+    var m = 0;
+    for(var i = 0; i < data.length; i++) {
+        var $img = document.createElement('img');
+        $img.src = data[i].img;
+        $img.onload = function() {
+            m++;
+            _this.arr.push(this);
+            arr.push(this);
+            if(m == data.length) {
+                _this.insertImage(arr);
+            }
+        }
+        $img.onerror = function() {
+            m++;
+            console.log(m)
+            if(m == data.length) {
+                _this.insertImage(arr);
+            }
+        }
+    }
+    // this.insertImage(data);
+}
