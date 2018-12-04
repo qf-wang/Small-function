bc248d64db3c17c8318457f885821c8368b1fe20
smallDuck <blivervat@gmail.com>
2018-12-04 23:31:17 +0800

    优化

--- deepCopy.js
+++ deepCopy.js
@@ -1,7 +1,8 @@
 var obj = {
   name: 'xiaolan',
   age: 18,
-  sister: [{
+  sister: [
+    {
       name: 'xixi',
       age: 24,
       hobby: ['逛街', '健身']
@@ -14,37 +15,60 @@ var obj = {
   ],
   eat: function () {
 
-  }
+  },
+  a: null,
+  arm: [1,2,3]
 }
 // 把一个对象的属性方法合并到另外一个对象上
-function deepCopy(obj, newObj) {
-  // 定义一个递归函数，如果属性为对象，继续执行
-  function deepFn(obj, newObj) {
-    for (let i in obj) {
-      // 属性为对象时
-      if (typeof obj[i] == 'object' && obj.hasOwnProperty(i)) {
-        // 如果obj的属性为对象，给newObj也添加一个属性为空对象，不然无法循环添加属性
-        newObj[i] = {};
-        deepFn(obj[i], newObj[i]);
-        // 属性添加完成，跳过这一步
-        continue;
-      }
-      // 属性不为对象时,直接赋值
-      newObj[i] = obj[i]
+// 相同属性如果数据类型不同或者数据类型为基本数据类型直接进行覆盖， 如果是引用数据类型，且数据类型一样，就进行结合
+function checkType(data) {
+  return Object.prototype.toString.call(data).match(/\w+/g)[1];
+}
+function deepCopy(copyObj, newObj) {
+  // 定义一个递归函数，如果属性为对象，继续执行,  如过是基本数据类型直接跳出
+  if(typeof copyObj != 'object' || copyObj == null) {
+      return copyObj;
+  }
+  // 如果传进来的新值数据类型为对象
+  if(checkType(newObj) != checkType(copyObj)) {
+    newObj = copyObj instanceof Array ? [] : {};  
+  }
+  for (let i in copyObj) {
+    if(copyObj.hasOwnProperty(i)) {
+      // 拷贝的为对象自有可枚举属性
+      newObj[i] = deepCopy(copyObj[i], newObj[i]);
     }
   }
-  // 设置默认值
-  newObj = newObj || {};
-  // 检测如果不是对象跳出
-  if (typeof newObj != 'object') {
-    return
-  }
-  deepFn(obj, newObj);
   return newObj;
 }
 var newObj = {
-  type: '属性合并'
+  type: '属性合并',
+  arm: {a: 'haha'},
+  sister: [1,2,3]
 }
 newObj = deepCopy(obj, newObj);
 console.log(newObj);
 console.log(obj);
+
+
+
+// function deepCopy(copyObj) {
+//   var newObj = copyObj instanceof Array ? [] : {};
+//   function deep(copyObj, newObj) {
+//     // 定义一个递归函数，如果属性为对象，继续执行
+//     for (let i in copyObj) {
+//       if (copyObj.hasOwnProperty(i)) {
+//         // 拷贝的为对象自有可枚举属性
+//         if (typeof copyObj[i] == 'object' && copyObj[i] != null) {
+//           newObj = copyObj instanceof Array ? [] : {};
+//           deepCopy(copyObj[i], newObj[i]);
+//           continue;
+//         }
+//         newObj[i] = copyObj[i];
+//       }
+//     }
+//     return newObj;
+//   }
+//   deep(copyObj, newObj);
+//   return newObj;
+// }
